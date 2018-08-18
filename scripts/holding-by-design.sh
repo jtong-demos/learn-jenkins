7a1ec0e6ab6fafe12dd70987167f088a81414836
Jian Tong <tj19832@gmail.com>
2018-08-18 23:58:09 +0800

    timeout

--- scripts/holding-by-design.sh
+++ scripts/holding-by-design.sh
@@ -1,6 +1,2 @@
-# s for seconds (the default,so you can simple input: sleep 2)
-# m for minutes.
-# h for hours.
-# d for days.
-
-sleep 2m
+echo "Hi, I'm sleeping for 2 minutes..."
+sleep 120 && echo "all done"
